5984ba2abae3d90f4690ad20f9f5119e0259a790
itsme <itsme@host.docker.internal>
2022-04-03 09:41:28 +0530

    Adding System Variables to pass parameters

--- SeleniumJenkinsTutorials/src/test/java/com/jenkins/demo/SeleniumJenkinsDemo.java
+++ SeleniumJenkinsTutorials/src/test/java/com/jenkins/demo/SeleniumJenkinsDemo.java
@@ -2,16 +2,27 @@ package com.jenkins.demo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SeleniumJenkinsDemo {
+	@Parameters("Browser")
 	@Test
-	public void validateTitleOrangeHRM() {
-		WebDriverManager.chromedriver().setup();
-		 WebDriver driver=new ChromeDriver();
+	public void validateTitleOrangeHRM(String browserName) {
+		System.out.println(browserName);
+		WebDriver driver = null;
+		if(browserName.contains("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			  driver=new ChromeDriver();
+		}
+		else if(browserName.contains("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			  driver=new EdgeDriver();
+		}
 		driver.manage().window().maximize();
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Assert.assertTrue(driver.getTitle().contains("Orange"), "Title does not match");
